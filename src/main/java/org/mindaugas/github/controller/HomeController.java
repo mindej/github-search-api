52cb4670096035b4d0a314e9a73a0e579176577b
Mindaugas Jaraminas <mindaugas.jaraminas1@ibm.com>
2020-09-22 13:13:33 +0300

    Added home controller and small updates

---
+++ src/main/java/org/mindaugas/github/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.mindaugas.github.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping("/")
+    public RedirectView redirecToSwagerUI() {
+        return new RedirectView("swagger-ui.html");
+    }
+}
